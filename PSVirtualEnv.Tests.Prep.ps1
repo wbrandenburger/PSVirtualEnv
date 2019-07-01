b07064d2a967b6089faa2b118d48694b85d24370
Wolfgang Brandenburger <wolfgang.brandenburger@live.de>
2019-07-01 17:12:07 +0200

    v0.2.4 create test environment at local system

---
+++ PSVirtualEnv.Tests.Prep.ps1
@@ -0,0 +1,81 @@
+# ==============================================================================
+#   PSVirtualEnv.Tests.Prep.ps1 ------------------------------------------------
+# ==============================================================================
+
+#   settings -------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+
+#   python distribution --------------------------------------------------------
+# ------------------------------------------------------------------------------
+    # get module name and directory
+    $Script:moduleName = "PSVirtualEnv"
+    $Script:moduleDir = Split-Path -Path $MyInvocation.MyCommand.Path -Parent
+
+    # check existing python distribution
+    Write-Host "PROCESS: Check existing python distribution." -ForegroundColor Yellow
+
+    $Script:pythonDir = $Env:PYTHONTEMP
+    if (-not  $Script:pythonDir -or -not (Test-Path -Path  $Script:pythonDir)){
+        Write-Host "ERROR: Please set environment variable 'PYTHONHOME' to existing python distribution (Python>3 is recommended)." -ForegroundColor Red
+        exit
+    }
+
+    $Script:python = Join-Path -Path $Script:pythonDir -ChildPath "python.exe"
+    if (-not (Test-Path -Path  $Script:python)){
+        Write-Host "ERROR: The python distribution seems to be corrupted, 'python.exe' was not found in python directory." -ForegroundColor Red
+        exit
+    }
+
+    Write-Host "SUCCESS: Python distribution detected: '$Script:python'." -ForegroundColor Green
+
+#   package virtualenv ---------------------------------------------------------
+# ------------------------------------------------------------------------------
+
+    # check whether package virtualenv exists in system pathon distribution
+    function Test-VirtualEnv {
+        $pythonPckg = . $Script:python -m pip list
+        if (-not ($pythonPckg  -match "virtualenv\s")) {
+            Write-Host "ERROR: Package virtualenv can not be found." -ForegroundColor Red
+
+            $choices  = "&Yes", "&No"
+            if ($Host.UI.PromptForChoice($Null, "Install package virtualenv from local?", $choices, 1) -eq 0) { 
+                Write-Host "PROCESS: Installation of package virtualenv." -ForegroundColor Yellow
+                $virtualEnvPckg = Get-ChildItem -Path (Join-Path -Path $moduleDir -ChildPath "Binaries\virtualenv") -Filter "*.whl" -File
+
+                . $Script:python -m pip install (Join-Path -Path $moduleDir -ChildPath "Binaries\virtualenv\$virtualEnvPckg")
+                
+                Test-VirtualEnv
+            } 
+            else { exit }        
+        }
+
+        Write-Host "SUCCESS: Pckg virtualenv detected." -ForegroundColor Green
+    }
+    
+    Test-VirtualEnv
+
+#   test environment -----------------------------------------------------------
+# ------------------------------------------------------------------------------
+
+    $Script:testWorkDir = "A:\Tests\PSVirtualEnv"
+    if (-not (Test-Path -Path $Script:testWorkDir)) {
+        New-Item -Path $Script:testWorkDir -ItemType Directory
+    }
+
+    $Script:testWorkSubDir = Get-ChildItem -Path $Script:testWorkDir -Directory
+
+    @(
+        "venv-1", "venv-2", "venv-3"
+    ) | ForEach-Object {
+        $Script:testEnvDir = Join-Path -Path $Script:testWorkDir -ChildPath $_
+
+        if (-not (Test-Path -Path  (Join-Path -Path $Script:testEnvDir -ChildPath "Scripts\python.exe"))) {
+            Remove-Item -Path $Script:testEnvDir -Recurse
+        }
+        if (-not (Test-Path -Path $Script:testEnvDir)) {
+            Write-Host "PROCESS: Creating the virtual environment '$_'." -ForegroundColor Yellow
+            . $Script:python -m virtualenv $Script:testEnvDir --verbose
+        }
+        
+        Write-Host "SUCCESS: Virtual environment '$_' exists." -ForegroundColor Green
+    }
---
+++ PSVirtualEnv.Tests.bat
@@ -0,0 +1,12 @@
+@echo off
+@setlocal enabledelayedexpansion
+@setlocal enableextensions
+
+REM ============================================================================
+REM PSVirtualEnv.Tests.bat -----------------------------------------------------
+REM ============================================================================
+
+call powershell -nologo -noprofile -file "%~dp0PSVirtualEnv.Tests.Prep.ps1"
+
+
+REM A:\Dropbox\Repositories\PSModule\PSVirtualEnv\PSVirtualEnv\PSVirtualEnv.Tests.bat
